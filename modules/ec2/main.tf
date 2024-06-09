ff85d575ecb3752a4e687d4cff604be68c9618ee
Siva Poreddy <srporeddy@gmail.com>
2024-06-09 17:31:55 -0500

    Update module to use variables for better behavior

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,4 +1,6 @@
 resource "aws_instance" "myec2" {
-   ami = "ami-0f30a9c3a48f3fa79"
-   instance_type = "t2.micro"
+   //ami = "ami-0f30a9c3a48f3fa79"
+   //instance_type = "t2.micro"
+   ami = var.my_ami_id
+   instance_type = var.my_instance_type
 }
---
+++ modules/ec2/variables.tf
@@ -0,0 +1,7 @@
+variable "my_ami_id" {
+    type = string
+}
+
+variable "my_instance_type" {
+    type = string
+}
